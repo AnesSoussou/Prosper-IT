52a615b42251d545c91bc9ea87cf539faa21f7e2
Knowledgecurrency <knowledgecurrency@gmail.com>
2024-02-20 22:22:28 +0100

    Ok

--- Prosper IT/src/pages/Chat/OpenAI.js
+++ Prosper IT/src/pages/Chat/OpenAI.js
@@ -4,7 +4,7 @@ const openAI = axios.create({
   baseURL: 'https://api.openai.com/v1',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_SECRET}`
+    'Authorization': `Bearer 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`
   }
 });
 
